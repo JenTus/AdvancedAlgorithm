032bb78de8c026c8bd4b6f06e8790df92d08f98b
sijing <tusijing007@gmail.com>
2018-03-25 17:40:45 +0300

    .

--- find_prime_power.py
+++ find_prime_power.py
@@ -1,3 +1,5 @@
+import math
+from sympy.ntheory.primetest import isprime
 # Given an integer N ∈ Z ≥2 as input, design an algorithm that either
 # (i) outputs a prime p and a positive integer a such that N = p a or
 # (ii) asserts that N is not a prime power.
@@ -5,20 +7,32 @@
 
 # Given integers N, k ∈ Z ≥2 as input, find the largest integer b
 # such that b ** k ≤ N .
+# when we find the exact b ** k == N, flag = 1
+# when we find the b **k < N, flag = 0
 def find_largest_b(N, k):
 
     def next_b(b_pre, b_next):
         if k ** b_next < N:
             return next_b(b_next, 2 * b_next)
         elif k ** b_next == N:
-            return b_next
+            return [1, b_next]
         else:
             if (b_pre + b_next) / 2 == b_pre:
-                return b_pre
+                return [0, b_pre]
             else:
                 return next_b(b_pre, (b_pre + b_next) / 2)
 
     return next_b(1, 1)
 
 
-find_largest_b(8, 2)
+# i^^b = N
+def find_prime_power(N):
+    for i in range(2, int(math.log(N) / math.log(2))):
+        [flag, b] = find_largest_b(N, i)
+        if flag == 1:
+            if isprime(i):
+                return [b, i]
+    return "N is not a prime power"
+
+
+print find_prime_power(1024)
